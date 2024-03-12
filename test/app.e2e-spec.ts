ae23c6bdf7ca9ddfcd332494eb4dec5545a25144
Talha Akber <talha_akber@hotmail.com>
2024-03-12 05:48:12 +0100

    test: adding e2e test coverage and adding mock data for axios api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,39 +2,63 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import { iqAirMockResponse } from './mock/mock-data.response';
+import { IQAirDataEntity } from '../src/iqair/entity/iqair-data.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { databaseProviders } from '../src/database/database.provider';
+import { MockType, repositoryMockFactory } from './mock/repository-mock.factory';
 
 describe('AppController (e2e)', () => {
 	let app: INestApplication;
+	let axiosMock: AxiosMockAdapter;
+	let repositoryMock: MockType<Repository<IQAirDataEntity>>;
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [AppModule]
+			imports: [AppModule],
+			providers: [
+				{ provide: getRepositoryToken(IQAirDataEntity), useFactory: repositoryMockFactory },
+			]
 		}).compile();
 
 		app = moduleFixture.createNestApplication();
+		axiosMock = new AxiosMockAdapter(axios);
+		axiosMock.onAny().reply(200, iqAirMockResponse); // I used onAny just for the showcase
+		repositoryMock = moduleFixture.get(getRepositoryToken(IQAirDataEntity));
 		await app.init();
 	});
 
-	it('/GET iqair - should return IQAir data for given coordinates', () => {
+	it('/GET pollution - should return IQAir data for given coordinates', () => {
 		return request(app.getHttpServer())
-			.get('/iqair')
-			.query({ lat: 35.98, lon: 140.33 })
+			.get('/pollution')
+			.query({ latitude: 35.98, longitude: 140.33 })
 			.expect(200)
-			.expect((response) => {
-				expect(response.body).toBeDefined();
-			});
+			.then(response => {
+				expect(response.body).toEqual({
+					result: {
+						pollution: {
+							aqicn: 21,
+							aqius: 29,
+							maincn: "n2",
+							mainus: "p2"
+						}
+					}
+				})
+			})
+			;
 	});
 
-	it('/iqair/most-polluted-date-time (GET)', () => {
+	it('/pollution/peak (GET)', () => {
 		return request(app.getHttpServer())
-			.get('/iqair/most-polluted-date-time')
+			.get('/pollution/peak')
 			.expect(200)
 			.expect((res) => {
-				const { mostPollutedDateTime } = res.body;
-				expect(mostPollutedDateTime).toBeDefined();
+				expect(res.body).toBeDefined();
 			});
 	});
-
 
 	afterAll(async () => {
 		await app.close();
---
+++ test/mock/mock-data.response.ts
@@ -0,0 +1,18 @@
+export const iqAirMockResponse = {
+    data: {
+        city: 'MockCity',
+        state: 'MockState',
+        country: 'MockCountry',
+        current: {
+            weather: {
+                ts: '2024-03-11T04:00:00.000Z',
+            },
+            pollution: {
+                aqius: 29,
+                mainus: 'p2',
+                aqicn: 21,
+                maincn: 'n2',
+            },
+        },
+    },
+};
